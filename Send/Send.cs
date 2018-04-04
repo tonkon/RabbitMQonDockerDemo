73ad09a4725943d13608a79410580a71cb8bfcbd
root <root@vmUbuntuProxy1.cstith455erudbthmp52it5g2d.ix.internal.cloudapp.net>
2018-04-04 15:06:05 +0000

    Add Send code

---
+++ Send/Send.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+class Send
+{
+    public static void Main(string[] args)
+    {
+	var mqhostname = Environment.GetEnvironmentVariable("RABBITMQ_HOST");
+	if (mqhostname.Length == 0)
+	{
+    	    Console.WriteLine("Please inject env variable RABBITMQ_HOST.");
+    	    return;
+	}
+
+	Console.WriteLine("Send Argument:" + mqhostname);
+        var factory = new ConnectionFactory() { HostName = mqhostname };
+        using(var connection = factory.CreateConnection())
+        using(var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            string message = "Hello World!";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
+            Console.WriteLine(" [x] Sent {0}", message);
+        }
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+}
